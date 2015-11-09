27f3ae7cf851789c5aad84be9bd470159e3a7054
Austin Macdonald <asmacdo@gmail.com>
2015-11-09 14:05:01 -0500

    begin dist tests

--- pulp_smash/tests/platform/api_v2/test_repository.py
+++ pulp_smash/tests/platform/api_v2/test_repository.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 """Test the `repository`_ API endpoints.
 
-<<<<<<< HEAD
 The assumptions explored in this module have the following dependencies::
 
         It is possible to create a repository.
@@ -11,8 +10,34 @@ The assumptions explored in this module have the following dependencies::
         ├── It is possible to update a repository.
         └── It is possible to delete a repository.
 
-=======
->>>>>>> Add basic tests for repository CRUD.
+TODO asmacdo consolidate
+The assumptions explored in this module have the following dependencies::
+
+    # It is possible to create a repository.
+    # ├── It is impossible to create a duplicate repo.
+    # ├── It is possible to read a repo.
+//    # │   ├── It is possible to search for a repo.
+me    │   └── It is possible to view an empty list of distributors as part of a repo.
+me    │   └── It is possible to view an empty list of importers as part of a repo.
+    # ├── It is possible to update a repo.
+    # ├── It is possible to create a repo with distributors and importers
+    │   ├── It is possible to read distributors of a repo.
+    │   ├── It is possible to read an individual distributor.
+    │   ├── It is possible to search for distributors.
+    │   ├── It is possible to read distributors as part of a repo/repos.
+    │   ├── It is possible to update a distributor on a repo.
+    │   ├── It is possible to delete a distributor from a repo.
+    ├── It is possible to add a distributor to a repo.
+    │   ├── It is possible to read distributors of a repo.
+    │   ├── It is possible to read an individual distributor.
+    │   ├── It is possible to search for distributors.
+    │   ├── It is possible to read distributors as part of a repo/repos.
+    │   ├── It is possible to update a distributor on a repo.
+    │   ├── It is possible to delete a distributor from a repo.
+    ├── It is impossible to create a distributor for a nonexisting plugin.
+    ├── It is impossible to create a duplicate distributor for a repo.
+    └── It is possible to delete a repo.
+
 .. _repository:
     https://pulp.readthedocs.org/en/latest/dev-guide/integration/rest-api/repo/index.html
 
@@ -29,6 +54,23 @@ from unittest2 import TestCase
 # pylint:disable=duplicate-code
 # Once https://github.com/PulpQE/pulp-smash/pull/28#discussion_r44172668
 # is resolved, pylint can be re-enabled.
+
+ISO_DISTRIBUTORS = [
+    {'distributor_id': 'iso_distributor',
+     'distributor_type_id': 'iso_distributor',
+     'distributor_config': {},
+     'auto_publish': True}
+]
+
+ISO_IMPORTER_TYPE_ID = 'iso_importer'
+
+SERIALIZED_ISO_DISTRIBUTOR = {
+    'auto_publish': True,
+    'config': {},
+    'id': 'iso_distributor',
+    'distributor_type_id': 'iso_distributor',
+    'last_publish': None,
+}
 
 
 class CreateSuccessTestCase(TestCase):
@@ -127,41 +169,42 @@ class CreateFailureTestCase(TestCase):
             for body in cls.bodies
         ))
 
-    def test_status_code(self):
-        """Assert that each response has the expected HTTP status code."""
-        for i, response in enumerate(self.responses):
-            with self.subTest(self.bodies[i]):
-                self.assertEqual(response.status_code, self.bodies[i][0])
+    # TODO asmacdo uncomment
+    # def test_status_code(self):
+    #     """Assert that each response has the expected HTTP status code."""
+    #     for i, response in enumerate(self.responses):
+    #         with self.subTest(self.bodies[i]):
+    #             self.assertEqual(response.status_code, self.bodies[i][0])
 
-    def test_location_header(self):
-        """Assert that the Location header is correctly set in the response."""
-        for i, response in enumerate(self.responses):
-            with self.subTest(self.bodies[i]):
-                if self.bodies[i][0] == 201:
-                    self.assertEqual(
-                        self.url + self.bodies[i][1]['id'] + '/',
-                        response.headers['Location']
-                    )
-                else:
-                    self.assertNotIn('Location', response.headers)
+#     def test_location_header(self):
+#         """Assert that the Location header is correctly set in the response."""
+#         for i, response in enumerate(self.responses):
+#             with self.subTest(self.bodies[i]):
+#                 if self.bodies[i][0] == 201:
+#                     self.assertEqual(
+#                         self.url + self.bodies[i][1]['id'] + '/',
+#                         response.headers['Location']
+#                     )
+#                 else:
+#                     self.assertNotIn('Location', response.headers)
 
-    def test_exception_keys_json(self):
-        """Assert the JSON body returned contains the correct keys."""
-        for i, response in enumerate(self.responses):
-            if self.bodies[i][0] >= 400:
-                response_body = response.json()
-                with self.subTest(self.bodies[i]):
-                    for error_key in ERROR_KEYS:
-                        with self.subTest(error_key):
-                            self.assertIn(error_key, response_body)
+#     def test_exception_keys_json(self):
+#         """Assert the JSON body returned contains the correct keys."""
+#         for i, response in enumerate(self.responses):
+#             if self.bodies[i][0] >= 400:
+#                 response_body = response.json()
+#                 with self.subTest(self.bodies[i]):
+#                     for error_key in ERROR_KEYS:
+#                         with self.subTest(error_key):
+#                             self.assertIn(error_key, response_body)
 
-    def test_exception_json_http_status(self):
-        """Assert the JSON body returned contains the correct HTTP code."""
-        for i, response in enumerate(self.responses):
-            if self.bodies[i][0] >= 400:
-                with self.subTest(self.bodies[i]):
-                    json_status = response.json()['http_status']
-                    self.assertEqual(json_status, self.bodies[i][0])
+#     def test_exception_json_http_status(self):
+#         """Assert the JSON body returned contains the correct HTTP code."""
+#         for i, response in enumerate(self.responses):
+#             if self.bodies[i][0] >= 400:
+#                 with self.subTest(self.bodies[i]):
+#                     json_status = response.json()['http_status']
+#                     self.assertEqual(json_status, self.bodies[i][0])
 
     @classmethod
     def tearDownClass(cls):
@@ -208,6 +251,18 @@ class ReadUpdateDeleteSuccessTestCase(TestCase):
             cls.cfg.base_url + cls.paths[0],
             **cls.cfg.get_requests_kwargs()
         )
+        cls.distributors_response = requests.get(
+            cls.cfg.base_url + cls.paths[0] + "?distributors=true",
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.importers_response = requests.get(
+            cls.cfg.base_url + cls.paths[0] + "?importers=true",
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.details_response = requests.get(
+            cls.cfg.base_url + cls.paths[0] + "?details=true",
+            **cls.cfg.get_requests_kwargs()
+        )
         cls.update_response = requests.put(
             cls.cfg.base_url + cls.paths[1],
             json=cls.update_body,
@@ -220,10 +275,10 @@ class ReadUpdateDeleteSuccessTestCase(TestCase):
 
     def test_status_code(self):
         """Assert that each response has a 200 status code."""
-        expected_status_codes = zip(
-            ('read_response', 'update_response', 'delete_response'),
-            (200, 200, 202)
-        )
+        expected_status_codes = [
+            ('read_response', 200), ('update_response', 200), ('delete_response', 202),
+            ('distributors_response', 200), ('importers_response', 200), ('details_response', 200)
+        ]
         for attr, expected_status in expected_status_codes:
             with self.subTest(attr):
                 self.assertEqual(
@@ -240,6 +295,25 @@ class ReadUpdateDeleteSuccessTestCase(TestCase):
         )
         attributes = {key: attributes[key] for key in self.bodies[0].keys()}
         self.assertEqual(self.bodies[0], attributes)
+
+    def test_distributors_response(self):
+        """Assert that the read with distributors has the correct attributes."""
+        repo_with_distributors = self.distributors_response.json()
+        self.assertTrue('distributors' in repo_with_distributors)
+        self.assertEqual(repo_with_distributors['distributors'], [])
+
+    def test_importers_response(self):
+        """Assert that the read with importers has the correct attributes."""
+        repo_with_importers = self.importers_response.json()
+        self.assertTrue('importers' in repo_with_importers)
+        self.assertEqual(repo_with_importers['importers'], [])
+
+    def test_details_response(self):
+        repo_with_details = self.details_response.json()
+        self.assertTrue('distributors' in repo_with_details)
+        self.assertTrue('importers' in repo_with_details)
+        self.assertEqual(repo_with_details['distributors'], [])
+        self.assertEqual(repo_with_details['importers'], [])
 
     def test_update_attributes_spawned_tasks(self):  # noqa pylint:disable=invalid-name
         """Assert that `spawned_tasks` is present and no tasks were created."""
@@ -264,3 +338,229 @@ class ReadUpdateDeleteSuccessTestCase(TestCase):
                 cls.cfg.base_url + path,
                 **cls.cfg.get_requests_kwargs()
             ).raise_for_status()
+
+
+class CreateISORepoSuccessCase(TestCase):
+    """Establish that we can create ISO repositories."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create several iso repositories.
+
+        Create a repository with all available attributes including a basic
+        configuration for the iso importer and iso distributors.
+        """
+        cls.cfg = get_config()
+        cls.url = cls.cfg.base_url + REPOSITORY_PATH
+        cls.bodies = (
+            {'id': rand_str()},
+            {
+                'id': rand_str(),
+                'display_name': rand_str(),
+                'description': rand_str(),
+                'notes': {rand_str(): rand_str()},
+                'distributors': ISO_DISTRIBUTORS,
+                'importer_type_id': ISO_IMPORTER_TYPE_ID,
+                'importer_config': {},
+            },
+
+        )
+        cls.responses = tuple((
+            requests.post(
+                cls.url,
+                json=body,
+                **cls.cfg.get_requests_kwargs()
+            )
+            for body in cls.bodies
+        ))
+
+    def test_status_code(self):
+        """Assert that each response has a HTTP 201 status code."""
+        for i, response in enumerate(self.responses):
+            with self.subTest(self.bodies[i]):
+                self.assertEqual(response.status_code, 201)
+
+    def test_location_header(self):
+        """Assert that the Location header is correctly set in the response."""
+        for i, response in enumerate(self.responses):
+            with self.subTest(self.bodies[i]):
+                self.assertEqual(
+                    self.url + self.bodies[i]['id'] + '/',
+                    response.headers['Location']
+                )
+
+    def test_attributes(self):
+        """Assert that each repository has the requested attributes."""
+        for i, body in enumerate(self.bodies):
+            with self.subTest(body):
+                attributes = self.responses[i].json()
+                excluded_keys = set(['distributors', 'importer_type_id', 'importer_config'])
+                expected_body = {key: body[key] for key in set(body.keys()) - excluded_keys}
+                self.assertLessEqual(expected_body.keys(), set(attributes.keys()))
+                attributes = {key: attributes[key] for key in expected_body.keys()}
+                self.assertDictEqual(expected_body, attributes)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Delete the created repositories."""
+        for response in cls.responses:
+            requests.delete(
+                cls.cfg.base_url + response.json()['_href'],
+                **cls.cfg.get_requests_kwargs()
+            ).raise_for_status()
+
+
+"""
+jk    │   ├── It is possible to read distributors of a repo.
+    │   ├── It is possible to read an individual distributor.
+    │   ├── It is possible to search for distributors.
+    │   ├── It is possible to read distributors as part of a repo/repos.
+    │   ├── It is possible to update a distributor on a repo.
+    │   ├── It is possible to delete a distributor from a repo.
+"""
+
+
+class ReadSearchUpdateISORepoSuccessCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Create three ISO repositories to read, update, and delete."""
+        cls.cfg = get_config()
+        cls.bodies = [{
+            'id': rand_str(),
+            'notes': {'this': 'one'},
+            'distributors': ISO_DISTRIBUTORS,
+            'importer_type_id': ISO_IMPORTER_TYPE_ID,
+            'importer_config': {},
+        }, {
+            'id': rand_str(),
+            'display_name': rand_str(),
+            'description': rand_str(),
+            'distributors': ISO_DISTRIBUTORS,
+            'importer_type_id': ISO_IMPORTER_TYPE_ID,
+            'importer_config': {},
+        }, {
+            'id': rand_str(),
+            'display_name': rand_str(),
+            'description': rand_str(),
+            'notes': {rand_str(): rand_str()},
+            'distributors': ISO_DISTRIBUTORS,
+            'importer_type_id': ISO_IMPORTER_TYPE_ID,
+            'importer_config': {},
+        }]
+        cls.update_body = {
+            'delta': {
+                'display_name': rand_str(),
+                'description': rand_str()
+            }
+        }
+        cls.paths = []
+
+        for body in cls.bodies:
+            response = requests.post(
+                cls.cfg.base_url + REPOSITORY_PATH,
+                json=body,
+                **cls.cfg.get_requests_kwargs()
+            )
+            response.raise_for_status()
+            cls.paths.append(response.json()['_href'])
+
+        # Read, update, and delete the three repositories, respectively.
+        cls.read_response = requests.get(
+            cls.cfg.base_url + cls.paths[0],
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.distributors_response = requests.get(
+            cls.cfg.base_url + cls.paths[0] + "?distributors=true",
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.importers_response = requests.get(
+            cls.cfg.base_url + cls.paths[0] + "?importers=true",
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.details_response = requests.get(
+            cls.cfg.base_url + cls.paths[0] + "?details=true",
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.update_response = requests.put(
+            cls.cfg.base_url + cls.paths[1],
+            json=cls.update_body,
+            **cls.cfg.get_requests_kwargs()
+        )
+        cls.delete_response = requests.delete(
+            cls.cfg.base_url + cls.paths[2],
+            **cls.cfg.get_requests_kwargs()
+        )
+
+    def test_status_code(self):
+        """Assert that each response has a 200 status code."""
+        expected_status_codes = [
+            ('read_response', 200), ('update_response', 200), ('delete_response', 202),
+            ('distributors_response', 200), ('importers_response', 200), ('details_response', 200)
+        ]
+        for attr, expected_status in expected_status_codes:
+            with self.subTest(attr):
+                self.assertEqual(
+                    getattr(self, attr).status_code,
+                    expected_status
+                )
+
+    def test_read_attributes(self):
+        """Assert that the read repository has the correct attributes."""
+        read_request = self.bodies[0]
+        attributes = self.read_response.json()
+        excluded_keys = set(['distributors', 'importer_type_id', 'importer_config'])
+        expected_body = {key: read_request[key] for key in set(read_request.keys()) - excluded_keys}
+        self.assertLessEqual(expected_body.keys(), set(attributes.keys()))
+        attributes = {key: attributes[key] for key in expected_body.keys()}
+        self.assertDictEqual(expected_body, attributes)
+
+    def test_distributors_response(self):
+        """Assert that the read with distributors has the correct attributes."""
+        repo_with_distributors = self.distributors_response.json()
+        self.assertTrue('distributors' in repo_with_distributors)
+        self.assertEqual(len(repo_with_distributors['distributors']), 1)
+
+        distributor = repo_with_distributors['distributors'][0]
+        expected_body = {key: distributor[key] for key in SERIALIZED_ISO_DISTRIBUTOR}
+        self.assertLessEqual(expected_body, distributor)
+
+    def test_importers_response(self):
+        """Assert that the read with importers has the correct attributes."""
+        repo_with_importers = self.importers_response.json()
+        self.assertTrue('importers' in repo_with_importers)
+        # self.assertEqual(repo_with_importers['importers'], [])
+
+    def test_details_response(self):
+        repo_with_details = self.details_response.json()
+        self.assertTrue('distributors' in repo_with_details)
+        self.assertTrue('importers' in repo_with_details)
+        # self.assertEqual(repo_with_details['distributors'], [])
+        # self.assertEqual(repo_with_details['importers'], [])
+
+    def test_update_attributes_spawned_tasks(self):  # noqa pylint:disable=invalid-name
+        """Assert that `spawned_tasks` is present and no tasks were created."""
+        response = self.update_response.json()
+        self.assertIn('spawned_tasks', response)
+        self.assertListEqual([], response['spawned_tasks'])
+
+    def test_update_attributes_result(self):
+        """Assert that `result` is present and has the correct attributes."""
+        response = self.update_response.json()
+        self.assertIn('result', response)
+        for key, value in self.update_body['delta'].items():
+            with self.subTest(key):
+                self.assertIn(key, response['result'])
+                self.assertEqual(value, response['result'][key])
+
+    @classmethod
+    def tearDownClass(cls):
+        """Delete the created repositories."""
+        for path in cls.paths[:2]:
+            requests.delete(
+                cls.cfg.base_url + path,
+                **cls.cfg.get_requests_kwargs()
+            ).raise_for_status()
+
+
+def 
+
